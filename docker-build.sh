4e773441023c03ae557b2b63fe055e022aea9030
Socheat <socheatsok78@gmail.com>
2019-10-04 14:21:33 +0700

    Update docker-build.sh, add build release

--- docker-build.sh
+++ docker-build.sh
@@ -7,6 +7,13 @@ image_tag="$image_org/cas:$image_version"
 echo "Building CAS docker image tagged as [$image_tag]"
 # read -p "Press [Enter] to continue..." any_key;
 
-docker build --tag="$image_tag" . \
+# docker build --tag="$image_tag" . \
+#   && echo "Built CAS image successfully tagged as $image_tag" \
+#   && docker images "$image_tag"
+
+if ! echo "$image_version" | grep -q "RC"; then
+  echo "Build latest release"
+  docker build --tag="$image_org/cas:latest" . \
   && echo "Built CAS image successfully tagged as $image_tag" \
   && docker images "$image_tag"
+fi
